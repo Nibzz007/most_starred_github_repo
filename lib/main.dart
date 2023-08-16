51727b549a5dc764b1408fc1c8571da703306758
Nibzz007 <nibukrishna07@gmail.com>
2023-08-16 10:21:46 +0530

    hive model created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:thiran_tech_task_2/controller/github_repo_data_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:thiran_tech_task_2/model/repo_hive_model.dart';
 import 'package:thiran_tech_task_2/view/splash_screen.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(GitHubRepoHiveAdapter());
+  await Hive.openBox<GitHubRepoHive>('repo');
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,13 +17,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => GitHubRepoDataProvider(),
-      child: MaterialApp(
+    return  MaterialApp(
         home: const SplashScreen(),
         theme: ThemeData(useMaterial3: true),
         debugShowCheckedModeBanner: false,
-      ),
-    );
+      );
   }
 }
